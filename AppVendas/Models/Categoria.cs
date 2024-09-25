e3e950d933ebeec22af6b4134492a4895dd06887
Rayssa <rayssa_nan.svl@hotmail.com>
2024-09-25 10:18:21 -0300

    Finalizado a estilização das Views Categorias

--- AppVendas/Models/Categoria.cs
+++ AppVendas/Models/Categoria.cs
@@ -1,8 +1,14 @@
-﻿namespace AppVendas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppVendas.Models
 {
     public class Categoria
     {
         public Guid CategoriaId { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo é obrigatório!")]
+        [MaxLength(100, ErrorMessage = "A descrição deve ter, no máximo, 100 caracteres!")]
         public string CategoriaNome { get; set; }
     }
 }
